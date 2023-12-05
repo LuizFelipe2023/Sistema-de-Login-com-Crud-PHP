06a14f668220ac3c50f5617dbf8565978eef57fc
LuizFelipe2023 <luizfelipefroisneves@gmail.com>
2023-12-05 11:45:44 -0400

    Versao 2.0

---
+++ database.sql
@@ -0,0 +1,21 @@
+
+CREATE DATABASE IF NOT EXISTS todo_list_app;
+
+
+USE todo_list_app;
+
+
+CREATE TABLE tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    status VARCHAR(50) NOT NULL,
+    userEmail VARCHAR(255) NOT NULL
+);
+
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
